b0d351c1a1a7e9e7a85d4d08a19c254bb74393cb
Mizael pParra <franciscomizaparra.24@gmail.com>
2022-05-12 16:45:02 -0500

    Punto Heroku 2

--- api/index.js
+++ api/index.js
@@ -13,6 +13,8 @@ app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.set('port', process.env.PORT || 3000)
+
 app.use('/api/users', users)
 
 const options = {
