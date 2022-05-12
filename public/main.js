b28f94c8b3a741d27ffb21b19aaf593be5640311
Mizael pParra <franciscomizaparra.24@gmail.com>
2022-05-12 16:41:48 -0500

    Punto Heroku

--- public/main.js
+++ public/main.js
@@ -1,5 +1,32 @@
-import { loadHeader } from "../templates/header.template.js"
-import { loadRegisterPage } from "../templates/login.template.js"
+const loadRegisterPage = () => {
+  const template = `
+    <div class="form-login-container">
+      <div class="form-login">
+        <div><img src="https://i.imgur.com/dFQdrud.png" /></div>
+        <form>
+          <input type="text" placeholder="Usuario" />
+          <input type="password" placeholder="Contraseña" />
+          <button>Iniciar Sesión</button>
+        </form>
+      </div>
+    </div>
+    <div class="notify-container"></div>
+  `
+  const section = document.getElementsByTagName('section')[0]
+  section.innerHTML = template
+}
+
+const loadHeader = () => {
+  const template = `
+    <div><a href="#" class="logo">Nutri App</a></div>
+    <ul class="h-menu">
+      <li><a href="#"><i class="icon fas fa-question"></i></a></li>
+      <li><a href="#"><i class="icon fas fa-cog"></i></a></li>
+    </ul>
+  `
+  const header = document.getElementsByTagName('header')[0]
+  header.innerHTML = template
+}
 
 window.onload = () => {
   loadHeader()
